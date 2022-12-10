febd9346c7aaa3c98b41a009006244e8e785fd5a
Daniil Rostilov <danilrostilov@gmail.com>
2022-12-10 22:48:26 +0300

    disable intersection

--- src/MouseCallbacks.js
+++ src/MouseCallbacks.js
@@ -1,5 +1,5 @@
 import { Point } from './Geometry';
-
+import { SceneObject } from './SceneObject';
 const MAX_ALLOWED_SPEED = 30.
 export class MouseState {
   constructor() {
@@ -43,6 +43,8 @@ export function move_callback(state, x, y, objects, last_speeds, walls, min_ind)
     let after_update_rect = objects[min_ind].rect.deep_copy();
     after_update_rect.tl.x = after_update_rect.tl.x + last_dx;
     after_update_rect.tl.y = after_update_rect.tl.y + last_dy;
+    const object_after_update = new SceneObject(after_update_rect);
+
     let is_intersecting = false;
     for (let j = 0; j < walls.length; j++) {
       if (walls[j].is_intersecting(after_update_rect)) {
@@ -50,9 +52,22 @@ export function move_callback(state, x, y, objects, last_speeds, walls, min_ind)
         break;
       }
     }
+
+    if (!is_intersecting) {
+      for (let j = 0; j < objects.length; j++) {
+        if (min_ind === j) {
+          continue;
+        }
+        if (objects[j].is_intersecting(object_after_update)) {
+          is_intersecting = true;
+        }
+      }
+    }
     if (is_intersecting) {
       last_speeds[min_ind] = new Point(0, 0);
     } else {
+
+
       objects[min_ind].rect = after_update_rect;
       last_speeds[min_ind] = new Point(last_dx, last_dy);
       const norm = last_speeds[min_ind].norm();
--- src/Render.js
+++ src/Render.js
@@ -12,9 +12,10 @@ export class ObjectsState {
 export function initialization(width, height, vid_width, vid_height) {
   let last_rects = [];
 
-  last_rects.push(new Rect(100, 100, vid_width / 2, vid_height));
-  last_rects.push(new Rect(400, 200, vid_width / 2, vid_height / 2));
-  last_rects.push(new Rect(550, 100, vid_width / 2, vid_height / 2));
+  last_rects.push(new Rect(30, 30, vid_width / 2, vid_height / 2));
+  last_rects.push(new Rect(400, 30, vid_width / 2, vid_height / 2));
+  last_rects.push(new Rect(30, 400, vid_width / 2, vid_height / 2));
+  last_rects.push(new Rect(400, 400, vid_width / 2, vid_height / 2));
 
   let objects = [];
   for (let i = 0; i < last_rects.length; i++) {
@@ -23,6 +24,7 @@ export function initialization(width, height, vid_width, vid_height) {
 
   let last_speeds = [];
 
+  last_speeds.push(new Point(0, 0));
   last_speeds.push(new Point(0, 0));
   last_speeds.push(new Point(0, 0));
   last_speeds.push(new Point(0, 0));
@@ -37,6 +39,7 @@ export function initialization(width, height, vid_width, vid_height) {
 
   tl_points.push(new Point(0, 0));
   tl_points.push(new Point(vid_width / 2, 0));
+  tl_points.push(new Point(0, vid_height / 2));
   tl_points.push(new Point(vid_width / 2, vid_height / 2));
 
   return [objects, last_speeds, walls, tl_points];
